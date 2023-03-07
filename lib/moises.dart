7e33077205b9d13990b7f077fef8981bf50da4e0
Gabriel Ramos <whosramoss@gmail.com>
2023-03-07 17:17:05 -0300

    add moises client

---
+++ lib/moises.dart
@@ -0,0 +1,32 @@
+library moises;
+
+/// api
+///
+export 'src/moises_api.dart';
+
+/// exception
+///
+export 'src/domain/exception/moises_exception.dart';
+
+/// models
+///
+export 'src/domain/models/moises_error.dart';
+export 'src/domain/models/moises_application_model.dart';
+export 'src/domain/models/moises_job_model.dart';
+export 'src/domain/models/moises_job_filter_model.dart';
+export 'src/domain/models/moises_new_job_model.dart';
+export 'src/domain/models/moises_new_job_params_model.dart';
+export 'src/domain/models/moises_new_job_result_model.dart';
+export 'src/domain/models/moises_workflow_graph_edge_model.dart';
+export 'src/domain/models/moises_workflow_graph_model.dart';
+export 'src/domain/models/moises_workflow_graph_node_data_model.dart';
+export 'src/domain/models/moises_workflow_graph_node_model.dart';
+export 'src/domain/models/moises_workflow_graph_node_position_model.dart';
+export 'src/domain/models/moises_workflow_params_model.dart';
+
+/// enums
+///
+export 'src/domain/enums/moises_worklflow_enum.dart';
+export 'src/domain/enums/moises_job_status_enum.dart';
+export 'src/domain/enums/media_input_codec_enum.dart';
+export 'src/domain/enums/media_output_codec_enum.dart';
---
+++ lib/src/moises_api.dart
@@ -0,0 +1,32 @@
+import 'package:moises/src/endpoints/application/i_moises_application.dart';
+import 'package:moises/src/endpoints/application/moises_application.dart';
+import 'package:moises/src/endpoints/job/i_moises_job.dart';
+import 'package:moises/src/endpoints/job/moises_job.dart';
+import 'package:moises/src/infrastructure/connect/moises_connect.dart';
+
+/// The `Moises` class.
+class Moises extends MoisesApiClient {
+  /// The instance of the `Moises` class.
+  Moises(super.apiKey);
+}
+
+/// The `MoisesApiClient` class.
+class MoisesApiClient {
+  /// The API key to use to authenticate with the API.
+  final String apiKey;
+
+  /// Create an http connection with the API
+  final MoisesConnect _connect;
+
+  /// The instance of the `MoisesApiClient` class.
+  MoisesApiClient(
+    this.apiKey, {
+    MoisesConnect? connect,
+  }) : _connect = connect ??= MoisesConnect.init(apiKey);
+
+  /// Getter method to fetch `Application` requests
+  IMoisesApplication get application => MoisesApplication(_connect);
+
+  /// Getter method to fetch `Job` requests
+  IMoisesJob get job => MoisesJob(_connect);
+}
