db7bb59b5b6e668df54f683223453f1796123545
Gabriel Ramos <whosramoss@gmail.com>
2025-01-24 18:54:01 -0300

    chore: remove exports

--- lib/moises.dart
+++ lib/moises.dart
@@ -15,7 +15,6 @@ export 'src/domain/models/moises_application_model.dart';
 export 'src/domain/models/moises_job_model.dart';
 export 'src/domain/models/moises_job_filter_model.dart';
 export 'src/domain/models/moises_new_job_model.dart';
-export 'src/domain/models/moises_new_job_params_model.dart';
 export 'src/domain/models/moises_new_job_result_model.dart';
 export 'src/domain/models/moises_workflow_params_model.dart';
 export 'src/domain/models/moises_workflow_filter_model.dart';
@@ -24,7 +23,6 @@ export 'src/domain/models/moises_upload_model.dart';
 
 /// enums
 ///
-export 'src/domain/enums/moises_worklflow_enum.dart';
 export 'src/domain/enums/moises_job_status_enum.dart';
 export 'src/domain/enums/media_input_codec_enum.dart';
 export 'src/domain/enums/media_output_codec_enum.dart';
